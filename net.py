2632abf697c3059a2a5df59f743098e0767b2362
Ernesto Elsäßer <ernesto.elsaesser@moovel.com>
2019-09-10 13:33:55 +0200

    better saving and loading

--- net.py
+++ net.py
@@ -2,6 +2,69 @@ import csv
 import numpy as np
 from numpy import array # required for eval in import function
 
+class BMIData:
+
+    def __init__(self, filename = "data_a_2_2016242.csv"):
+        self.xs = np.zeros((0,6))
+        self.ys = np.zeros((0,2))
+
+        with open(filename, newline='') as file:
+            data = csv.reader(file, delimiter=';')
+            next(data) # skip first lineexit
+            for row in data:
+                gender = 1 if row[0] == 'w' else 0
+                height = self.normalize(int(row[1]), 140, 200)
+                age = self.normalize(int(row[2]), 18, 100)
+                weight = self.normalize(int(row[3]), 20, 150)
+                strength_sports = 1 if row[4] == 'Kraftsport' else 0
+                endurance_sports = 1 if row[4] == 'Ausdauersport' else 0
+                underweight = 1 if row[5] == 'Untergewicht' else 0
+                overweight = 1 if row[5] == 'Uebergewicht' else 0
+                self.xs = np.append(self.xs, [[gender, height, age, weight, strength_sports, endurance_sports]], 0)
+                self.ys = np.append(self.ys, [[underweight, overweight]], 0)
+
+    def normalize(self, value, upper, lower):
+        normalized = (value - lower) / (upper - lower)
+        return min(max(normalized, 0), 1)
+
+class BMINet:
+
+    def __init__(self):
+        self.net = FeedForwardNet(6, 15, 2, 0.01, 0.3)
+
+    def load(self):
+        with open("trained.net", 'r') as file:
+            raw = file.read()
+            self.net.deserialize(raw)
+
+    def save(self):
+        with open("trained.net", 'w') as file:
+            raw = self.net.serialize()
+            file.write(raw)
+
+    def train(self, data, range_start, range_end):
+        start = np.datetime64('now')
+        train_range = range(range_start, range_end)
+        self.net.train(data.xs, data.ys, train_range)
+        end = np.datetime64('now')
+        print("Trainingsdauer: " + str(end - start))
+
+    def test(self, data, range_start, range_end):
+        test_range = range(range_start, range_end)
+        (correct, _) = self.net.test(data.xs, data.ys, test_range)
+        count = len(test_range)
+        accuracy = correct / count
+        print("{0}/{1} korrekt ({2:.0%})".format(correct, count, accuracy))
+
+    def classify(self, data, index):
+        (_, out) = self.net.apply(data.xs[index])
+        if (1 - out[0] < self.net.epsilon):
+            return "Untergewicht"
+        if (1 - out[1] < self.net.epsilon):
+            return "Uebergewicht"
+        return "Normalgewicht"
+
+
 class FeedForwardNet:
 
     def __init__(self, in_count, hidden_count, out_count, epsilon, learning_rate):
@@ -80,64 +143,11 @@ class FeedForwardNet:
 
         return (correct, total_error)
     
-    def export(self, filename = "trained.net"):
-        with open(filename, 'w') as file:
-            file.write(repr(self.in_to_hidden_weights).replace("\n", "") + "\n")
-            file.write(repr(self.hidden_to_out_weights).replace("\n", "") + "\n")
+    def serialize(self):
+        array = [self.in_to_hidden_weights, self.hidden_to_out_weights]
+        return repr(array)
 
-    def import_(self, filename = "trained.net"):
-        with open(filename, 'r') as file:
-            self.in_to_hidden_weights = eval(file.readline())
-            self.hidden_to_out_weights = eval(file.readline())
-
-class BMIData:
-
-    def __init__(self, filename = "data_a_2_2016242.csv"):
-        self.xs = np.zeros((0,6))
-        self.ys = np.zeros((0,2))
-
-        with open(filename, newline='') as file:
-            data = csv.reader(file, delimiter=';')
-            next(data) # skip first lineexit
-            for row in data:
-                gender = 1 if row[0] == 'w' else 0
-                height = self.normalize(int(row[1]), 140, 200)
-                age = self.normalize(int(row[2]), 18, 100)
-                weight = self.normalize(int(row[3]), 20, 150)
-                strength_sports = 1 if row[4] == 'Kraftsport' else 0
-                endurance_sports = 1 if row[4] == 'Ausdauersport' else 0
-                underweight = 1 if row[5] == 'Untergewicht' else 0
-                overweight = 1 if row[5] == 'Uebergewicht' else 0
-                self.xs = np.append(self.xs, [[gender, height, age, weight, strength_sports, endurance_sports]], 0)
-                self.ys = np.append(self.ys, [[underweight, overweight]], 0)
-
-    def normalize(self, value, upper, lower):
-        normalized = (value - lower) / (upper - lower)
-        return min(max(normalized, 0), 1)
-
-class BMINet:
-
-    def __init__(self):
-        self.net = FeedForwardNet(6, 15, 2, 0.01, 0.3)
-
-    def train(self, data, range_start, range_end):
-        start = np.datetime64('now')
-        train_range = range(range_start, range_end)
-        self.net.train(data.xs, data.ys, train_range)
-        end = np.datetime64('now')
-        print("Trainingsdauer: " + str(end - start))
-
-    def test(self, data, range_start, range_end):
-        test_range = range(range_start, range_end)
-        (correct, _) = self.net.test(data.xs, data.ys, test_range)
-        count = len(test_range)
-        accuracy = correct / count
-        print("{0}/{1} korrekt ({2:.0%})".format(correct, count, accuracy))
-
-    def classify(self, data, index):
-        (_, out) = self.net.apply(data.xs[index])
-        if (1 - out[0] < self.net.epsilon):
-            return "Untergewicht"
-        if (1 - out[1] < self.net.epsilon):
-            return "Uebergewicht"
-        return "Normalgewicht"
+    def deserialize(self, raw):
+        array = eval(raw)
+        self.in_to_hidden_weights = array[0]
+        self.hidden_to_out_weights = array[1]
