c1fdf7edc2590cc5d722b451a69b3e6c5f0c222c
Ernesto Elsäßer <ernesto.elsaesser@moovel.com>
2019-09-10 13:21:52 +0200

    refactored code

--- net.py
+++ net.py
@@ -2,14 +2,14 @@ import csv
 import numpy as np
 from numpy import array # required for eval in import function
 
-class NeuralNetwork:
+class FeedForwardNet:
 
     def __init__(self, in_count, hidden_count, out_count, epsilon, learning_rate):
         self.in_count = in_count
         self.hidden_count = hidden_count
         self.out_count = out_count
-        self.syn0 = 2 * np.random.random((in_count + 1, hidden_count)) - 1
-        self.syn1 = 2 * np.random.random((hidden_count + 1, out_count)) - 1
+        self.in_to_hidden_weights = 2 * np.random.random((in_count + 1, hidden_count)) - 1
+        self.hidden_to_out_weights = 2 * np.random.random((hidden_count + 1, out_count)) - 1
         self.epsilon = epsilon
         self.learning_rate = learning_rate
 
@@ -18,43 +18,52 @@ class NeuralNetwork:
 
     def transfer_deriv(self, x):
         return x*(1-x)
-    
-    def feed(self, x, y, backpropagate):
-        l0p = x
-        l0 = np.append(l0p, 1)
-        l1p = self.transfer(np.dot(l0, self.syn0))
-        l1 = np.append(l1p, 1)
-        l2 = self.transfer(np.dot(l1, self.syn1))
-        l2_error = y - l2
 
-        error_sum = np.sum(np.square(l2_error)) / 2
+    def compute_layer(self, inputs, weights):
+        inputs_ext = np.append(inputs, 1)
+        outputs = self.transfer(np.dot(inputs_ext, weights))
+        return outputs
 
-        if backpropagate and error_sum > self.epsilon:
-            l2_delta = l2_error * self.transfer_deriv(l2)
-            syn1p = np.delete(self.syn1, self.hidden_count, 0)
-            l1_error = l2_delta.dot(syn1p.T)
-            l1_delta = l1_error * self.transfer_deriv(l1p)
-            syn1_delta = self.learning_rate * np.outer(l1p, l2_delta)
-            syn0_delta = self.learning_rate * np.outer(l0p, l1_delta)
-            self.syn1 += np.append(syn1_delta, [np.zeros(self.out_count)], 0)
-            self.syn0 += np.append(syn0_delta, [np.zeros(self.hidden_count)], 0)
+    def apply(self, x):
+        hidden_layer = self.compute_layer(x, self.in_to_hidden_weights)
+        out_layer = self.compute_layer(hidden_layer, self.hidden_to_out_weights)
+        return (hidden_layer, out_layer)
 
-        return error_sum
+    def overall_error(self, outputs, y):
+        return np.sum(np.square(y - outputs)) / 2
+
+    def adjust_weights(self, inputs, outputs, error, weights):
+        scaled_error = error * self.transfer_deriv(outputs)
+        delta = self.learning_rate * np.outer(inputs, scaled_error)
+        delta_ext = np.append(delta, [np.zeros(outputs.shape[0])], 0)
+        weights += hidden_to_out_delta_ext # TODO test
+
+    def backpropagate(self, x, hidden, out, y):
+        out_error = y - out
+        self.adjust_weights(hidden, out, out_error, self.hidden_to_out_weights)
+        scaled_out_error = out_error * self.transfer_deriv(out)
+        weights_without_tresholds = np.delete(self.hidden_to_out_weights, self.hidden_count, 0)
+        hidden_error= scaled_out_error.dot(weights_without_tresholds.T)
+        self.adjust_weights(x, hidden, hidden_error, self.in_to_hidden_weights)
 
     def train(self, xs, ys, sample_range):
         round = 0
-        wrong_classifications = True
-        while (wrong_classifications):
+        learned_all = False
+        while (not learned_all):
             round += 1
 
             for i in sample_range:
                 x = xs[i]
                 y = ys[i]
-                while self.feed(x, y, True) > self.epsilon:
-                    continue
+                (hidden, out) = self.apply(x)
+                overall_error = self.overall_error(out, y)
+                while overall_error > self.epsilon:
+                    self.backpropagate(x, hidden, out, y)
+                    (hidden, out) = self.apply(x)
+                    overall_error = self.overall_error(out, y)
 
             (correct, total_error) = self.test(xs, ys, sample_range)
-            wrong_classifications = correct < len(sample_range)
+            learned_all = correct == len(sample_range)
 
             print("Runde " + str(round) + " - Korrekte: " + str(correct) + " Fehler : " + str(total_error))
 
@@ -63,28 +72,27 @@ class NeuralNetwork:
         total_error = 0
 
         for i in sample_range:
-            error = self.feed(xs[i], ys[i], False)
-            total_error += error
-            if (error < self.epsilon):
+            (_, out) = self.apply(xs[i])
+            overall_error = self.overall_error(out, y)
+            if (overall_error < self.epsilon):
                 correct += 1
+            total_error += overall_error
 
         return (correct, total_error)
     
     def export(self, filename = "trained.net"):
         with open(filename, 'w') as file:
-            file.write(repr(self.syn0).replace("\n", "") + "\n")
-            file.write(repr(self.syn1).replace("\n", "") + "\n")
+            file.write(repr(self.in_to_hidden_weights).replace("\n", "") + "\n")
+            file.write(repr(self.hidden_to_out_weights).replace("\n", "") + "\n")
 
     def import_(self, filename = "trained.net"):
         with open(filename, 'r') as file:
-            self.syn0 = eval(file.readline())
-            self.syn1 = eval(file.readline())
+            self.in_to_hidden_weights = eval(file.readline())
+            self.hidden_to_out_weights = eval(file.readline())
 
 class BMIData:
 
-    def __init__(self, train_count = 100, test_count = 500, filename = "data_a_2_2016242.csv"):
-        self.train_range = range(0, train_count)
-        self.test_range = range(train_count, train_count + test_count)
+    def __init__(self, filename = "data_a_2_2016242.csv"):
         self.xs = np.zeros((0,6))
         self.ys = np.zeros((0,2))
 
@@ -107,17 +115,29 @@ class BMIData:
         normalized = (value - lower) / (upper - lower)
         return min(max(normalized, 0), 1)
 
-    def train(self, net):
+class BMINet:
+
+    def __init__(self):
+        self.net = FeedForwardNet(6, 15, 2, 0.01, 0.3)
+
+    def train(self, data, range_start, range_end):
         start = np.datetime64('now')
-        net.train(self.xs, self.ys, self.train_range)
+        train_range = range(range_start, range_end)
+        self.net.train(data.xs, data.ys, train_range)
         end = np.datetime64('now')
         print("Trainingsdauer: " + str(end - start))
 
-    def test(self, net):
-        (correct, _) = net.test(self.xs, self.ys, self.test_range)
-        count = len(self.test_range)
+    def test(self, data, range_start, range_end):
+        test_range = range(range_start, range_end)
+        (correct, _) = self.net.test(data.xs, data.ys, test_range)
+        count = len(test_range)
         accuracy = correct / count
         print("{0}/{1} korrekt ({2:.0%})".format(correct, count, accuracy))
 
-def make():
-    return NeuralNetwork(6, 15, 2, 0.01, 0.3)
+    def classify(self, data, index):
+        (_, out) = self.net.apply(data.xs[index])
+        if (1 - out[0] < self.net.epsilon):
+            return "Untergewicht"
+        if (1 - out[1] < self.net.epsilon):
+            return "Uebergewicht"
+        return "Normalgewicht"
